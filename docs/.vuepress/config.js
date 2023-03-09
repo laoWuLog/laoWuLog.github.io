539333dbb42b6bcf3c2755153d3305b121727eba
wuxianwei <wuxianwei@interlib.vip>
2023-03-09 15:47:46 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  dest: '../',
+  title: '个人播客',
+  description: 'xxxxxxx',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '老吴的 JavaScript 博客',
+        items: [
+          { text: 'Github', link: 'https://github.com/mqyqingfeng' }
+        ]
+      }
+    ]
+  }
+}
